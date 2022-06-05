65fde10cf2a4147a175cafd4e43268e896909eb9
Bambosh <Sebastianmbsmith@gmail.com>
2022-06-06 07:58:00 +1000

    Initial Commit

---
+++ Assets/Creator Kit - RPG/Scripts/Gameplay/InputController.cs
@@ -0,0 +1,62 @@
+using RPGM.Core;
+using RPGM.Gameplay;
+using UnityEngine;
+
+namespace RPGM.UI
+{
+    /// <summary>
+    /// Sends user input to the correct control systems.
+    /// </summary>
+    public class InputController : MonoBehaviour
+    {
+        public float stepSize = 0.1f;
+        GameModel model = Schedule.GetModel<GameModel>();
+
+        public enum State
+        {
+            CharacterControl,
+            DialogControl,
+            Pause
+        }
+
+        State state;
+
+        public void ChangeState(State state) => this.state = state;
+
+        void Update()
+        {
+            switch (state)
+            {
+                case State.CharacterControl:
+                    CharacterControl();
+                    break;
+                case State.DialogControl:
+                    DialogControl();
+                    break;
+            }
+        }
+
+        void DialogControl()
+        {
+            model.player.nextMoveCommand = Vector3.zero;
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+                model.dialog.FocusButton(-1);
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+                model.dialog.FocusButton(+1);
+            if (Input.GetKeyDown(KeyCode.Space))
+                model.dialog.SelectActiveButton();
+        }
+
+        void CharacterControl()
+        {
+            float xDir = 0;
+            float yDir = 0;
+            if (Input.GetKey(KeyCode.UpArrow)) yDir++;
+            if (Input.GetKey(KeyCode.DownArrow)) yDir--;
+            if (Input.GetKey(KeyCode.RightArrow)) xDir++;
+            if (Input.GetKey(KeyCode.LeftArrow)) xDir--;
+
+            model.player.nextMoveCommand = new Vector3(xDir * stepSize, yDir * stepSize, 0);
+        }
+    }
+}
